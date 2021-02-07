8e38887039894601278da7277fecbdec1994a260
br3nnan8 <54257787+br3nnan8@users.noreply.github.com>
2021-02-07 17:43:25 -0500

    update notebooks, init login

---
+++ login.py
@@ -0,0 +1,12 @@
+def sign_in(email, password):
+#     this function automates login process on linkedin, 
+#     provide email and password as strings
+
+    # webdriver gets page
+    driver.get("https://www.linkedin.com/")
+    # pause for page to load
+    time.sleep(3)
+    # locate and send login email and password
+    driver.find_element_by_id("session_key").send_keys(email)
+    driver.find_element_by_id("session_password").send_keys(password)
+    driver.find_element_by_class_name("sign-in-form__submit-button").click()
